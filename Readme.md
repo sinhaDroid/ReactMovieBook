2c248f05781c34e8c00592924249f68249452d47
Deepanshu Sinha <107sinha@gmail.com>
2019-01-08 00:14:29 +0530

    prettier and eslint added

---
+++ Readme.md
@@ -0,0 +1,6 @@
+# ReactMovieBook
+
+## Setup Prettier
+
+Add .eslintrc
+Setup prettier - npm i prettier eslint-config-prettier eslint-plugin-prettier -D
--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,6 +1,6 @@
-import React from 'react';
-import Header from '../elements/Header/Header';
-import Home from '../Home/Home';
+import React from "react";
+import Header from "../elements/Header/Header";
+import Home from "../Home/Home";
 
 const App = () => {
   return (
@@ -8,7 +8,7 @@ const App = () => {
       <Header />
       <Home />
     </div>
-  )
-}
+  );
+};
 
 export default App;
