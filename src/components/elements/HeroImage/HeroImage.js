c5c38f0b7629d4646ba97ae30f77fd703f37d4cb
Deepanshu Sinha <107sinha@gmail.com>
2019-01-08 23:14:53 +0530

    eslint rules changed, HeroImage elements added

---
+++ src/components/elements/HeroImage/HeroImage.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./HeroImage.css";
+
+class HeroImage = (props) => {
+    return (
+        <div>
+            HeroImage
+        </div>
+    )
+}
+
+export default HeroImage;
